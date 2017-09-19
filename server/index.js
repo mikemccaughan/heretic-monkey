d7418c92fa91d359e251b918ac5fdd9b5a32382e
Mike McCaughan <1719723+mikemccaughan@users.noreply.github.com>
2017-09-18 21:27:01 -0400

    Adding build stuff

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const website = process.env.website || './dist';
+const app = express();
+
+app.use(express.static(website));
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: website });
+});
+
+const port = process.env.port || '80';
+app.listen(port, () => console.log(`app running on port ${port}`));
