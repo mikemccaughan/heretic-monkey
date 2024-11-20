4d4796946f35ffa6e50eb647dadca1726829a311
Mike McCaughan <github@mikemccaughan.com>
2024-11-20 14:30:02 -0500

    Updates to use PM2 to run express

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import path from 'node:path';
+import os from 'node:os';
 const website = process.env.website || path.resolve('../dist/browser');
 const app = express();
+const hostname = os.hostname();
 console.log(website);
 app.use(express.static(website));
 app.get('*', (req, res) => {
   res.sendFile('index.html', { root: website });
 });
 
-const port = process.env.port || '8118';
-app.listen(port, () => console.log(`app running on port ${port}; http://localhost:${port}/`));
+const port = process.env.PORT || '8118';
+const scheme = '' + port === '443' ? 'https' : 'http';
+app.listen(port, () => console.log(`app running on port ${port}; ${scheme}://${hostname}:${port}/`));
