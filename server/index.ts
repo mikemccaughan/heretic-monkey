aff1d9e5139591b39123a97adc94bd84c9395d7b
Mike McCaughan <github@mikemccaughan.com>
2025-10-28 16:01:16 -0400

    Update server code; package.json

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,17 @@ import path from 'node:path';
 import os from 'node:os';
 const website = process.env.website || path.resolve('../dist/browser');
 const app = express();
-const hostname = os.hostname();
-console.log(website);
+const host = os.hostname();
 app.use(express.static(website));
-app.get('*', (req, res) => {
+app.get('/#/{*splat}', async (req: { params: { splat: string; }}, res) => {
+  console.log(req.params.splat, 'good');
+  res.sendFile(`index.html/#/${req.params.splat}`, { root: website });
+});
+app.get('/{*splat}', async (req: { params: { splat: string; }}, res) => {
+  console.log(req.params.splat, 'bad');
   res.sendFile('index.html', { root: website });
 });
 
 const port = process.env.PORT || '8118';
 const scheme = '' + port === '443' ? 'https' : 'http';
-app.listen(port, () => console.log(`app running on port ${port}; ${scheme}://${hostname}:${port}/`));
+app.listen(port, () => console.log(`app running on port ${port}; ${scheme}://${host}:${port}/`));
